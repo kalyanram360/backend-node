1ac50aef17e67622fceb5e399d8b0d00bf950100
kalyan <mkalyan8400@gmail.com>
2025-05-25 22:57:40 +0530

    todays commit

--- projectn/src/controlers/user.controler.js
+++ projectn/src/controlers/user.controler.js
@@ -1,9 +1,28 @@
 import { asynchandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+
 const registerUser = asynchandler(async (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "User fuck",
+  const { fullName, email, username, password } = req.body;
+  console.log(fullName, email, username, password);
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  User.findOne({
+    $or: [{ email }, { username }],
+  }).then((user) => {
+    if (user) {
+      throw new ApiError(
+        409,
+        "User already exists with this email or username"
+      );
+    }
   });
+
+  req.files?.avatar[0], path;
 });
 
 export { registerUser };
--- projectn/src/routes/user.routes.js
+++ projectn/src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controlers/user.controler.js";
+import { upload } from "../middleware/multer.middleware.js";
+
+import(upload);
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
